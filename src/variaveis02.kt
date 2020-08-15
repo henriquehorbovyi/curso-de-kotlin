682e288e9a1be866301867ab0e58b1d1edd6583d
Henrique Horbovyi <carlos.souza@paygo.com.br>
2020-08-14 23:37:49 -0300

    Adicionado aula 1 e aula 2

---
+++ src/variaveis02.kt
@@ -0,0 +1,27 @@
+fun main() {
+    // Inteiros (int) (1,2,3,4)
+    val anoAtual = 2020
+    val proximoAno: Int = 2021
+
+    // Float (1.5, 2.7, 5.6)
+    val altura = 1.77f
+    val pesoDaCadeira: Float = 4.5f
+
+    // Double > Float: O Double nos permite trabalhar com números maiores
+    val PI: Double = 3.14159265359
+
+    // Char - Um unico caractere
+    val primeiraLetraDoMeuNome: Char = 'H'
+
+    // String - Armazenam textos, palavras, vários caracteres de uma só vez
+    val nome = "Lucas"
+    val sobrenome: String = "Silva"
+
+
+    // Booleans
+    val voceEhAlto = false
+    val voceTrabalha: Boolean = true
+
+    val ehMaior: Boolean = 3 == 3
+    println(ehMaior) // 3 é 3 então 'ehMaior' será 'true'
+}
