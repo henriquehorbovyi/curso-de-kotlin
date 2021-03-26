5e701efa9430885a7d73454d6d1e52851e64745a
Henrique Horbovyi <henrikhorbovyi@gmail.com>
2021-03-26 14:15:32 -0300

    refatora o projeto para o novo template do intelliJ

---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.4.10"
+}
+
+group = "io.henrikhorbovyi"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test-junit"))
+}
+
+tasks.test {
+    useJUnit()
+}
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "1.8"
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "curso-de-kotlin"
