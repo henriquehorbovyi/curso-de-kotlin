de7fae0b99014db62da37c506c047b85a3660f51
Henrique Horbovyi <henrikhorbovyi@gmail.com>
2021-04-02 15:59:00 -0300

    adiciona código da aula 08

---
+++ src/main/kotlin/08lacoWhile.kt
@@ -0,0 +1,55 @@
+fun main() {
+
+
+    /*while (true) {
+        println("Ao infinito e além")
+    }*/
+
+    /*var contador = 0
+    while (contador < 5) {
+        println("Ao infinito e além, não tão infinito assim")
+        contador++
+    }
+    */
+
+    /*var opcao = 0
+    while (opcao != 2) {
+        println("Escolha uma opção: (1) Iniciar Jogo | (2) Sair")
+        opcao = readLine()!!.toInt()
+
+        if (opcao == 1) {
+            println("Iniciando o jogo...")
+        }
+    }
+    println("Jogo finalizado!")*/
+
+
+    /*var opcao = 0
+    while (true) {
+        println("Escolha uma opção: (1) Iniciar Jogo | (2) Sair")
+        opcao = readLine()!!.toInt()
+
+        if (opcao == 1) {
+            println("Iniciando o jogo...")
+        } else {
+          break
+        }
+    }*/
+
+
+    while (false) {
+        println("Nunca vou existir ;(")
+    }
+
+    do {
+        println("vai ser executado uma vez")
+    } while (false)
+
+
+
+
+
+
+
+
+}
